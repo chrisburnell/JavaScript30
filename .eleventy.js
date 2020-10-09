bc19f6770774c5c43931db93fd939ede194573fe
Chris Burnell <me@chrisburnell.com>
2020-10-09 11:47:26 +0100

    markdownFormat baebee

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,9 @@
 const { DateTime } = require("luxon");
+const markdownIt = require('markdown-it')({
+    html: true,
+    breaks: true,
+    linkify: true
+});
 
 module.exports = function(config) {
     const now = new Date();
@@ -17,11 +22,16 @@ module.exports = function(config) {
     config.addFilter("dateFormat", (value, format = "dd LLLL yyyy") => {
         return DateTime.fromJSDate(new Date(value)).toFormat(format);
     });
+
     config.addFilter("w3DateFormat", (value, showTimezone = true) => {
         if (showTimezone) {
             return DateTime.fromJSDate(new Date(value)).toFormat("yyyy-MM-dd'T'HH:mm:ssZZZ");
         }
         return DateTime.fromJSDate(new Date(value)).toFormat("yyyy-MM-dd'T'HH:mm:ss");
+    });
+
+    config.addFilter("markdownFormat", (value) => {
+        return markdownIt.render(value);
     });
 
     return {
